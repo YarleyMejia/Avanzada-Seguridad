dd735eded9c1cf868264fcf369363b87064e04e2
YarleyMejia <100544029+YarleyMejia@users.noreply.github.com>
2025-04-05 21:49:39 -0500

    se sube la estructura generar de la rama master dadas las dificultades de integracion presentadas.

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Avanzada-Seguridad"
+
---
+++ src/main/java/co/edu/uniquindio/proyecto/modelo/documentos/Comentario.java
@@ -0,0 +1,29 @@
+package co.edu.uniquindio.proyecto.modelo.documentos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document("comentarios")
+public class Comentario {
+    @Id
+    private ObjectId id;
+    private ObjectId reporteId;
+    private String mensaje;
+    private LocalDateTime fecha;
+    private ObjectId clienteId;
+
+    public Comentario(String mensaje, LocalDateTime fecha, ObjectId clienteId) {
+        this.mensaje = mensaje;
+        this.fecha = fecha;
+        this.clienteId = clienteId;
+    }
+}
