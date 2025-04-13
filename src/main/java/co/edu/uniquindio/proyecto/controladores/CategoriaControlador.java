fc467ad55aa12e7c485ec0c7864d38ea748ab1f2
Juanesarangoi <jearangoi@uqvirtual.edu.co>
2025-04-13 01:03:29 -0500

    Se implementan todos los requerimientos de las Categorias

--- src/main/java/co/edu/uniquindio/proyecto/controladores/CategoriaControlador.java
+++ src/main/java/co/edu/uniquindio/proyecto/controladores/CategoriaControlador.java
@@ -1,4 +1,44 @@
 package co.edu.uniquindio.proyecto.controladores;
 
+import co.edu.uniquindio.proyecto.dto.CrearCategoriaDTO;
+import co.edu.uniquindio.proyecto.dto.EditarCategoriaDTO;
+import co.edu.uniquindio.proyecto.dto.EliminarCategoriaDTO;
+import co.edu.uniquindio.proyecto.modelo.documentos.Categoria;
+import co.edu.uniquindio.proyecto.servicios.interfaces.CategoriaServicio;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categorias")
 public class CategoriaControlador {
+
+    private CategoriaServicio categoriaServicio;
+
+    @PostMapping
+    public Categoria crear(@Valid @RequestBody CrearCategoriaDTO dto) {
+        return categoriaServicio.crear(dto);
+    }
+
+    @PutMapping
+    public Categoria editar(@Valid @RequestBody EditarCategoriaDTO dto) {
+        return categoriaServicio.editar(dto);
+    }
+
+    @DeleteMapping
+    public void eliminar(@Valid @RequestBody EliminarCategoriaDTO dto) {
+        categoriaServicio.eliminar(dto);
+    }
+
+    @GetMapping("/{id}")
+    public Categoria buscarPorId(@PathVariable String id) {
+        return categoriaServicio.buscarPorId(id);
+    }
+
+    @GetMapping
+    public List<Categoria> listar() {
+        return categoriaServicio.listar();
+    }
 }
---
+++ src/main/java/co/edu/uniquindio/proyecto/dto/EditarCategoriaDTO.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.proyecto.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record EditarCategoriaDTO(
+        @NotBlank String descripcion,
+        @NotBlank String nombre,
+        @NotBlank String id
+) {
+}
